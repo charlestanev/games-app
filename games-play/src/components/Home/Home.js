b28fe783ee0c8b44192a8918f3da504302e191d9
ELMIR TANEV <51314768+charlestanev@users.noreply.github.com>
2022-11-10 10:59:42 +0200

    List latest games from server
    List latest games from server

--- games-play/src/components/Home/Home.js
+++ games-play/src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import * as gameService from '../../services/gameService.js';
+import LatestGame from "./LatestGame/LatestGame.js";
 
 const Home = () => {
 	const [games, setGames] = useState([]);
@@ -7,6 +8,7 @@ const Home = () => {
 	useEffect(() => {
 		gameService.getAll()
 			.then(result => {
+				setGames(result);
 				console.log(result);
 			})
 	}, [])
@@ -21,60 +23,7 @@ const Home = () => {
 			<div id="home-page">
 				<h1>Latest Games</h1>
 				{/* Display div: with information about every game (if any) */}
-				<div className="game">
-					<div className="image-wrap">
-						<img src="./images/CoverFire.png" />
-					</div>
-					<h3>Cover Fire</h3>
-					<div className="rating">
-						<span>☆</span>
-						<span>☆</span>
-						<span>☆</span>
-						<span>☆</span>
-						<span>☆</span>
-					</div>
-					<div className="data-buttons">
-						<a href="#" className="btn details-btn">
-							Details
-						</a>
-					</div>
-				</div>
-				<div className="game">
-					<div className="image-wrap">
-						<img src="./images/ZombieLang.png" />
-					</div>
-					<h3>Zombie Lang</h3>
-					<div className="rating">
-						<span>☆</span>
-						<span>☆</span>
-						<span>☆</span>
-						<span>☆</span>
-						<span>☆</span>
-					</div>
-					<div className="data-buttons">
-						<a href="#" className="btn details-btn">
-							Details
-						</a>
-					</div>
-				</div>
-				<div className="game">
-					<div className="image-wrap">
-						<img src="./images/MineCraft.png" />
-					</div>
-					<h3>MineCraft</h3>
-					<div className="rating">
-						<span>☆</span>
-						<span>☆</span>
-						<span>☆</span>
-						<span>☆</span>
-						<span>☆</span>
-					</div>
-					<div className="data-buttons">
-						<a href="#" className="btn details-btn">
-							Details
-						</a>
-					</div>
-				</div>
+				{games.map(x => <LatestGame game={x} key={games._ownerId} />)}
 				{/* Display paragraph: If there is no games  */}
 				<p className="no-articles">No games yet</p>
 			</div>
